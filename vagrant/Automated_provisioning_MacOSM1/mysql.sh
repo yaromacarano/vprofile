68f192924b47d2d53a9b60fe81612854ed383e73
yaromacarano <kurinnoy890@gmail.com>
2025-09-08 16:03:33 +0200

    random changes

--- vagrant/Automated_provisioning_MacOSM1/mysql.sh
+++ vagrant/Automated_provisioning_MacOSM1/mysql.sh
@@ -11,7 +11,7 @@ sudo systemctl start mariadb
 sudo systemctl enable mariadb
 cd /tmp/
 git clone -b main https://github.com/hkhcoder/vprofile-project.git
-#restore the dump file for the application
+#restore thecdsefscfs dump file for the application
 sudo mysqladmin -u root password "$DATABASE_PASS"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
